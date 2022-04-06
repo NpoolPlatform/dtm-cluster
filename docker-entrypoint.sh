8b35f29a28378d9b7411481792129042456f3d4a
fxnt <1330672764@qq.com>
2022-04-06 21:20:56 +0800

    add dtm

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -32,3 +32,5 @@ ls /app/dtm/configs/
 sed -i "s/HOST/$MYSQL_HOST/g" /app/dtm/configs/config.yaml
 sed -i "s/PORT/$MYSQL_PORT/g" /app/dtm/configs/config.yaml
 sed -i "s/PWD/$MYSQL_PASSWORD/g" /app/dtm/configs/config.yaml
+
+/app/dtm/dtm
